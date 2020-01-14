a5149863c92008ade6accd251bc7811e0731814f
Laszlo <44098779+LaszloWa@users.noreply.github.com>
2020-01-14 23:06:55 +0100

    implemented delete functionality for profile, so that user can delete their profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,6 +7,26 @@ const Profile = ({ user, isDarkMode, onRouteChange }) => {
     } else {
         isTextColor = 'black-80';
     }
+
+    const deleteProfile = (user) => {
+        fetch('https://sleepy-wildwood-42035.herokuapp.com/deleteProfile', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: user.email,
+            })
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response === 'success') {
+                    onRouteChange('signIn');
+                    alert('User successfully deleted')
+                } else {
+                    alert('Sorry, something went wrong, please try again.')
+                }
+            })
+    }
+
     return (
         <article className="br3 ba shadow-5 b--black-10 mv4 w-200 w-50-m w-25-l mw6 center">
             <main className={`pa4 ${isTextColor}`}>
@@ -16,7 +36,7 @@ const Profile = ({ user, isDarkMode, onRouteChange }) => {
                     <p>Joined: <br/>{user.joined}</p>
                 </div>
                 <div className="">
-                    <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Delete user" onClick={'boo'} />
+                    <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Delete user" onClick={() => deleteProfile(user)} />
                 </div>
                 <div className="lh-copy mt3">
                     <p className="f6 link black db pointer grow" onClick={() => onRouteChange('home')}>Back</p>
